c88a77f5b322830f92b220991e81e8d794dba007
Galin-Kotsev <kocevi900701@gmail.com>
2022-09-29 00:20:31 +0300

    update 29.09.2022

---
+++ 1.Programing Basics C#/4.For -цикъл/MORE EXERCISE/09. Clock/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09._Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int a = 0; a <= 59; a++)
+                {
+
+                    Console.WriteLine($"{i} : {a}");
+
+                }
+            }
+        }
+    }
+}
---
+++ 1.Programing Basics C#/4.For -цикъл/MORE EXERCISE/10. Clock - part 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace _10._Clock___part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int a = 0; a <= 59; a++)
+                {
+                    for (int b = 0; b <= 59; b++)
+                    {
+                        Console.WriteLine($"{i} : {a} : {b}");
+                    }
+                }
+            }
+        }
+    }    
+}

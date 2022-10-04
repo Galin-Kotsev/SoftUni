90597bd3af903f8ba488997b46d509769f4c09fd
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-05 00:10:11 +0300

    05.10.2022

---
+++ 1.Programing Basics C#/6.Вложени цикли/More Exercise/02. Letters Combinations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02._Letters_Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            int combinations = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    for (int k = a; k <= b; k++)
+                    {
+                        if (i != c && j!= c && k != c)
+                        {
+                            Console.Write($"{(char)i}{(char)j}{(char)k} ");
+                            combinations++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(combinations);
+        }
+    }
+}
+    
+
---
+++ 1.Programing Basics C#/6.Вложени цикли/More Exercise/03. Lucky Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03._Lucky_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum1 = 0;
+            int sum2 = 0;   
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    for (int g = 1; g <= 9; g++)
+                    {
+                        for (int h = 1; h <= 9; h++)
+                        {
+                            sum1 = i + k;
+                            sum2 = g + h;
+
+                            if (sum1 == sum2)
+                            {
+                                if (n%sum1 == 0)
+                                {
+                                    Console.Write($"{i}{k}{g}{h} ");
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

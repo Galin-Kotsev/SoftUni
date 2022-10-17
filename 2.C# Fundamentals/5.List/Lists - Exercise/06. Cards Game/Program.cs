ec29fad811b347a595f27125ce02f2d9703bf442
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-18 00:30:42 +0300

    18.10.2022

---
+++ 2.C# Fundamentals/5.List/Lists - Exercise/06. Cards Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Cards_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> PlayerOne = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> PlayerTwo = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+
+            while (true)
+            {
+                if (PlayerOne.Count == 0 || PlayerTwo.Count== 0)
+                {
+                    if (PlayerOne.Count != 0)
+                    {
+                        Console.WriteLine($"First player wins! Sum: {PlayerOne.Sum()}");
+                        break;
+                    }
+                    else if (PlayerTwo.Count != 0) 
+                    {
+                        Console.WriteLine($"Second player wins! Sum: {PlayerTwo.Sum()}");
+                        break;
+                    }
+                }
+
+                if (PlayerOne[0] > PlayerTwo[0])
+                {
+                    PlayerOne.Add(PlayerTwo[0]);
+                    PlayerOne.Add(PlayerOne[0]);
+
+                    PlayerTwo.Remove(PlayerTwo[0]);
+                    PlayerOne.Remove(PlayerOne[0]);
+                }
+                else if (PlayerTwo[0] > PlayerOne[0])
+                {
+                    PlayerTwo.Add(PlayerOne[0]);
+                    PlayerTwo.Add(PlayerTwo[0]);
+
+                    PlayerOne.Remove(PlayerOne[0]);
+                    PlayerTwo.Remove(PlayerTwo[0]);
+                }
+                else if (PlayerOne[0] == PlayerTwo[0])
+                {
+                    PlayerOne.Remove(PlayerOne[0]);
+                    PlayerTwo.Remove(PlayerTwo[0]);
+                }
+
+            }
+        }
+    }
+}
---
+++ 2.C# Fundamentals/5.List/Lists - Exercise/4. List Operations/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._List_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+            
+            while (input != "End")
+            {
+                string[] inputs = input.Split();
+                string command = inputs[0];
+                string integer = inputs[1];
+
+                if (command == "Add")
+                {
+                    numbers.Add(int.Parse(integer));
+                }
+                else if (command == "Remove")
+                {
+                    if (int.Parse(integer) >= 0 && int.Parse(integer) < numbers.Count)
+                    {
+                        numbers.RemoveAt(int.Parse(integer));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                }
+                else if (command == "Insert")
+                {
+                    if (int.Parse(inputs[2]) >= 0 && int.Parse(inputs[2]) < numbers.Count)
+                    {
+                        numbers.Insert(int.Parse(inputs[2]), int.Parse(integer));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                }
+                else if (integer == "left")
+                {
+                    int couts = int.Parse(inputs[2]);
+
+                    for (int i = 0; i < couts ; i++)
+                    {
+                        numbers.Add(numbers[0]);
+                        numbers.RemoveAt(0);
+                    }
+                    
+                }
+                else if (integer == "right")
+                {
+                    int couts = int.Parse(inputs[2]);
+
+                    for (int i = 0; i < couts; i++)
+                    {
+                        numbers.Insert(0, numbers[numbers.Count - 1]);
+                        numbers.RemoveAt(numbers.Count - 1);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}

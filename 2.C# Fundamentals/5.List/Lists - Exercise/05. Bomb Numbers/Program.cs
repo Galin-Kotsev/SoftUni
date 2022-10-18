c89fa08e0b1fe3068568100a4206cddffee4ca8f
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-18 23:53:11 +0300

    18.10.2022

---
+++ 2.C# Fundamentals/5.List/Lists - Exercise/05. Bomb Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int bomb = input[0];
+            int power = input[1];
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb)
+                {
+                    for (int j = i - power; j <= i + power; j++)
+                    {
+                        if (j < 0)
+                        {
+                            j = 0;
+                        }
+                        if (j > numbers.Count - 1)
+                        {
+                            break;
+                        }
+                        numbers[j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ 2.C# Fundamentals/5.List/Lists - Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+                
+
+            List<int> numbers2 = new List<int>();
+
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                List<int> reversed = numbers[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                numbers2.AddRange(reversed);
+            }
+            
+            Console.WriteLine(String.Join(" ", numbers2));
+        }
+    }
+}

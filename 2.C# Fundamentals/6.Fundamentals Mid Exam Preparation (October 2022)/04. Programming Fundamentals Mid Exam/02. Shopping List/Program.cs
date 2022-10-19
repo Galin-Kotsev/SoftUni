963ced68cda8f7a08edea44b41e000c06c2d9c63
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-20 00:56:08 +0300

    20.10.2022 Exam_Preparation

---
+++ 2.C# Fundamentals/6.Fundamentals Mid Exam Preparation (October 2022)/04. Programming Fundamentals Mid Exam/02. Shopping List/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine()
+                .Split("!")
+                .ToList();
+
+            string input = Console.ReadLine();
+             
+
+
+            while (input != "Go Shopping!")
+            {
+                string[] inputs = input.Split();
+                string command = inputs[0];
+                string item = inputs[1];
+
+
+                if (command == "Urgent")
+                {
+                    if (!groceries.Contains(item))
+                    {
+                        groceries.Insert(0, item);
+                    }
+                }
+                else if (command == "Unnecessary")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Remove(item);
+                    }
+                }
+                else if (command == "Correct")
+                {
+
+                    if (groceries.Contains(item))
+                    {
+                        string item2 = inputs[2];
+
+                        groceries[groceries.IndexOf(item)] = item2;
+                    }
+                }
+                else if (command == "Rearrange")
+                {
+                    if (groceries.Contains(item))
+                    {
+                        groceries.Add(item);
+                        groceries.Remove(item);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ",groceries));
+        }
+    }
+}
---
+++ 2.C# Fundamentals/6.Fundamentals Mid Exam Preparation (October 2022)/04. Programming Fundamentals Mid Exam/03. Heart Delivery/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Heart_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> heartsInHouse = Console.ReadLine()
+                .Split("@")
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            int position = 0;
+
+            while (command != "Love!")
+            {
+                string[] inpust = command.Split();
+                int jumps = int.Parse(inpust[1]);
+
+                if (position + jumps < heartsInHouse.Count)
+                {
+                    position += jumps;
+                }
+                else
+                {
+                    position = 0;
+                }
+
+                heartsInHouse[position] -= 2;
+
+                if (heartsInHouse[position] == 0)
+                {
+                    Console.WriteLine($"Place {position} has Valentine's day.");
+                    heartsInHouse[position] = -1;
+                }
+                else if (heartsInHouse[position] < 0)
+                {
+                    Console.WriteLine($"Place {position} already had Valentine's day.");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            int fail = 0;
+            for (int i = 0; i < heartsInHouse.Count; i++)
+            {
+                if (heartsInHouse[i] > 0)
+                {
+                    fail++;
+                }
+            }
+
+            Console.WriteLine($"Cupid's last position was {position}.");
+            if (fail == 0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {fail} places.");
+            }
+
+        }
+
+
+    }
+}
+

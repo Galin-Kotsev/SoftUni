c543dad52fd22000e54a08c31ae635c15a43c5ce
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-26 00:23:23 +0300

    26.10.2022

--- 2.C# Fundamentals/06.Mid Exam Preparation (October 2022)/01. PF Mid Exam Retake/01. Computer Store/Program.cs
+++ 2.C# Fundamentals/06.Mid Exam Preparation (October 2022)/01. PF Mid Exam Retake/01. Computer Store/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace _01._Computer_Store
 {
@@ -7,7 +8,59 @@ namespace _01._Computer_Store
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            double partTax = 0;
+            double totalPriceNoTax = 0;
+
+            while (input != "special" && input != "regular")
+            {
+                double partPrice = double.Parse(input);
+
+                if (partPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                else
+                {
+                    partTax += partPrice * 0.20;
+                    totalPriceNoTax += partPrice;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "special")
+            {
+                if (totalPriceNoTax == 0)
+                {
+                    Console.WriteLine("Invalid order!");
+                }
+                else
+                {
+                    double totalPriceWithTax = (partTax + totalPriceNoTax) * 0.90;
+                    Console.WriteLine($"Congratulations you've just bought a new computer!");
+                    Console.WriteLine($"Price without taxes: {totalPriceNoTax:f2}$");
+                    Console.WriteLine($"Taxes: {partTax:f2}$");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine($"Total price: {totalPriceWithTax:f2}$");
+                }
+            }
+            else
+            {
+                if (totalPriceNoTax == 0)
+                {
+                    Console.WriteLine("Invalid order!");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations you've just bought a new computer!");
+                    Console.WriteLine($"Price without taxes: {totalPriceNoTax:f2}$");
+                    Console.WriteLine($"Taxes: {partTax:f2}$");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine($"Total price: {partTax + totalPriceNoTax:f2}$");
+                }
+            }
         }
     }
 

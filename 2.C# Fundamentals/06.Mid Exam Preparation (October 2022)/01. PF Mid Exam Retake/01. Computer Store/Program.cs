75f8442e2844e1542ea043745a08f4d16d1ec940
Galin-Kotsev <kocevi900701@gmail.com>
2022-10-25 23:28:33 +0300

    25.10.2022

---
+++ 2.C# Fundamentals/06.Mid Exam Preparation (October 2022)/01. PF Mid Exam Retake/01. Computer Store/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
---
+++ 2.C# Fundamentals/06.Mid Exam Preparation (October 2022)/01. PF Mid Exam Retake/02. The Lift/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passenger = int.Parse(Console.ReadLine());
+
+            int[] lifts = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            bool full = false;
+
+            for (int i = 0; i < lifts.Length; i++)
+            {
+                while (lifts[i] < 4 && passenger > 0)
+                {
+                    lifts[i]++;
+                    passenger--;
+                }
+
+                if (lifts[lifts.Length - 1] == 4)
+                {
+                    full = true;
+                }
+            }
+
+            if (passenger > 0 && full)
+            {
+                Console.WriteLine($"There isn't enough space! {passenger} people in a queue!");
+                Console.WriteLine(String.Join(" ", lifts));
+
+            }
+            else if (passenger == 0 && !full)
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(String.Join(" ", lifts));
+            }
+            else if (passenger == 0 && full)
+            {
+                Console.WriteLine(String.Join(" ", lifts));
+            }
+
+        }
+    }
+}

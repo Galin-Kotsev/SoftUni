f491ad862a18c106a30f7226fde2f2904e9adfe1
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-03 23:15:34 +0200

    03.11.2022

---
+++ 2.C# Fundamentals/08.Objects and Classes/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Advertisement_Message
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Phrases = new List<string>
+            {"Excellent product.", "Such a great product.", 
+             "I always use that product.", "Best product of its category.", 
+             "Exceptional product.", "I can’t live without this product."};
+
+            List<string> Events = new List<string>
+            {"Now I feel good.", "I have succeeded with this product.",
+             "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.",
+             "Try it yourself, I am very satisfied.", "I feel great!"};
+
+            List<string> Authors = new List<string>
+            {"Diana", "Petya", "Stella", "Elena",
+             "Katya", "Iva", "Annie", "Eva"};
+
+            List<string> Cities = new List<string>
+            {"Burgas", "Sofia", "Plovdiv",
+             "Varna", "Ruse"};
+
+            int n = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{Phrases[random.Next(0, Phrases.Count)]} {Events[random.Next(0, Events.Count)]} {Authors[random.Next(0, Authors.Count)]} - {Cities[random.Next(0, Cities.Count)]}");
+                
+            }
+
+           
+        }
+    }
+}
---
+++ 2.C# Fundamentals/08.Objects and Classes/Objects and Classes - Exercise/06. Vehicle Catalogue/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace _06._Vehicle_Catalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Catalogue carInfo = new Catalogue();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] VehicleInfo = input.Split(" ");
+
+                string type = VehicleInfo[0];
+                string model = VehicleInfo[1];
+                string color = VehicleInfo[2];
+                int horsePower = int.Parse(VehicleInfo[3]);
+
+                if (type == "car")
+                {
+                    Car car = new Car(model, color, horsePower);
+                    carInfo.Cars.Add(car);
+                }
+                else
+                {
+                    Truck truck = new Truck(model, color, horsePower);
+                    carInfo.Trucks.Add(truck);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string input2 = Console.ReadLine();
+
+            while (input2 != "Close the Catalogue")
+            {
+                if (carInfo.Cars.Any(x => x.Model  == input2))
+                {
+                    Car car = carInfo.Cars.First(x => x.Model == input2);
+                    Console.WriteLine("Type: Car");
+                    Console.WriteLine($"Model: {car.Model}");
+                    Console.WriteLine($"Color: {car.Color}");
+                    Console.WriteLine($"Horsepower: {car.Horsepower}");
+                }
+
+                if (carInfo.Trucks.Any(x => x.Model == input2))
+                {
+                    Truck truck = carInfo.Trucks.First(x => x.Model == input2);
+                    Console.WriteLine("Type: Truck");
+                    Console.WriteLine($"Model: {truck.Model}");
+                    Console.WriteLine($"Color: {truck.Color}");
+                    Console.WriteLine($"Horsepower: {truck.Horsepower}");
+                }
+
+
+                input2 = Console.ReadLine();
+            }
+
+            if (carInfo.Cars.Count > 0)
+            {
+                Console.WriteLine($"Cars have average horsepower of: {carInfo.Cars.Average(x => x.Horsepower):f2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Cars have average horsepower of: 0.00.");
+            }
+
+            if (carInfo.Trucks.Count > 0)
+            {
+                Console.WriteLine($"Trucks have average horsepower of: {carInfo.Trucks.Average(x => x.Horsepower):f2}.");
+            }
+            else
+            {
+                Console.WriteLine($"Trucks have average horsepower of: 0.00.");
+            }
+        }
+    }
+}
+public class Catalogue
+{
+    public Catalogue()
+    {
+        Trucks = new List<Truck>();
+        Cars = new List<Car>();
+    }
+
+    public List<Truck> Trucks { get; set; }
+    public List<Car> Cars { get; set; }
+}
+public class Car
+{
+    public Car(string model, string color, int horsePower)
+    {
+        Model = model;
+        Color = color;
+        Horsepower = horsePower;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Horsepower { get; set; }
+}
+public class Truck
+{
+    public Truck(string model, string color, int horsepower)
+    {
+        Model = model;
+        Color = color;
+        Horsepower = horsepower;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Horsepower { get; set; }
+}

11579080001ba3b036d48712523a0e4b91298e06
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-05 00:28:06 +0200

    05.11.2022

---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - EXERCISE/01. Count Chars in a String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+            foreach (char ch in input)
+            {
+                if (ch != ' ')
+                {
+                    if (!words.ContainsKey(ch))
+                    {
+                        words[ch] = 0;
+                    }
+
+                    words[ch]++;
+                }
+            }
+
+            foreach (var (Key, Value) in words)
+            {
+                Console.WriteLine($"{Key} -> {Value}");
+            }
+            
+        }
+    }
+}
---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - EXERCISE/02. A Miner Task/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mine = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "stop")
+            {
+                if (!mine.ContainsKey(input))
+                {
+                    mine.Add(input, 0);
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+                mine[input] += quantity;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var (Key, Value) in mine)
+            {
+                Console.WriteLine($"{Key} -> {Value}");
+            }
+        }
+
+    }
+}

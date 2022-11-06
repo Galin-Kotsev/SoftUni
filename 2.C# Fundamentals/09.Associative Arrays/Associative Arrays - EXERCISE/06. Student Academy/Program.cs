c9e39986ae515f824cdb543fd653f326bfec4312
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-07 00:00:24 +0200

    07.11.2022

---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - EXERCISE/06. Student Academy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _06._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var gradesList = new Dictionary<string, List<double>>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string student = Console.ReadLine();  
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!gradesList.ContainsKey(student))
+                {
+                    gradesList.Add(student, new List<double>());
+                }
+
+                gradesList[student].Add(grade);
+            }
+
+            foreach (var (Key, Value) in gradesList)
+            {
+                if (Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{Key} -> {Value.Average():f2}");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - EXERCISE/07. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[]inputs = input.Split(" -> ");
+
+                string company = inputs[0];
+                string id = inputs[1];
+
+                if (!employeeList.ContainsKey(company))
+                {
+                    employeeList.Add(company, new List<string>());
+                }
+
+                if (!employeeList[company].Contains(id))
+                {
+                    employeeList[company].Add(id);
+                } 
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var (company, id) in employeeList)
+            {
+                Console.WriteLine(company);
+
+                foreach (var item in id)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}

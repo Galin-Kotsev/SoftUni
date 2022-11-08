1b5acd663fc81f354861e521c410a5c61a7b5316
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-09 00:05:17 +0200

    08.11.2022

---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - MORE EXERCISE/01. Ranking/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Ranking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contestList = new Dictionary<string, string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end of contest")
+            {
+                string[] inputs = input.Split(":");
+
+                contestList.Add(inputs[0], inputs[1]);
+                
+                input = Console.ReadLine();
+            }
+
+            List<Contests> contests = new List<Contests>();  
+
+            string input2 = Console.ReadLine();
+
+            while (input2 != "end of submissions")
+            {
+                string[] inputs = input2.Split("=>");
+                string contest = inputs[0];
+                string password = inputs[1];
+                string userName = inputs[2];
+                int points = int.Parse(inputs[3]);
+
+                if (contestList.ContainsKey(contest))
+                {
+                    if (password == contestList[contest])
+                    {
+                        if (contests.Any(x => x.Contest == contest))
+                        {
+
+                        }
+                        
+                    }
+                }
+
+
+
+
+
+                input2 = Console.ReadLine();
+            }
+        }
+    }
+}
+public class Contests
+{
+    public Contests(string contest, string password, string username, int points)
+    {
+        Contest = contest;
+        Password = password;
+        Username = username;
+        Points = points;
+    }
+    
+    public string Contest { get; set; }
+    public string Password { get; set; }
+    public string Username { get; set; }
+    public int Points { get; set; }
+
+}
+
+
+// to be continued
---
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - MORE EXERCISE/02. Judge/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Judge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contests> contests = new List<Contests>();
+            Dictionary<string, int> users = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "no more time")
+            {
+                string[]inputs = input.Split(" -> ");
+                string userName = inputs[0];
+                string contest = inputs[1];
+                int points = int.Parse(inputs[2]);
+
+                
+
+                if (!contests.Any(x => x.Name == contest))
+                {
+                    contests.Add(new Contests(contest));
+                }
+
+                Contests selectedContest = contests.First(x => x.Name == contest);
+
+                if (!selectedContest.User.ContainsKey(userName))
+                {
+                    selectedContest.User.Add(userName, points);
+                }
+                else if (selectedContest.User[userName] < points)
+                {
+                    selectedContest.User[userName] = points;
+                  
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var contest in contests)
+            {
+                foreach (var user in contest.User)
+                {
+                    if (!users.ContainsKey(user.Key))
+                    {
+                        users.Add(user.Key, user.Value);
+                    }
+                    else
+                    {
+                        users[user.Key] += user.Value;
+                    }
+                }
+            }
+
+            foreach (var item in contests)
+            {
+                Console.WriteLine($"{item.Name}: {item.User.Count()} participants");
+
+                int counter = 1;
+
+                foreach (var (Key, Value) in item.User.OrderByDescending((x => x.Value)).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{counter}. {Key} <::> {Value}");
+                    counter++;
+                }
+            }
+
+            int count = 1;
+
+            Console.WriteLine($"Individual standings:");
+            foreach (var (User, Points) in users.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{count}. {User} -> {Points}");
+
+                count++;
+            }
+        }
+    }
+}
+class Contests
+{
+    public Contests(string contest)   
+    {
+        Name = contest;
+        User = new Dictionary<string, int>();
+    }
+
+    public string Name { get; set; }
+    public Dictionary<string, int> User { get; set; }
+
+}
+
+

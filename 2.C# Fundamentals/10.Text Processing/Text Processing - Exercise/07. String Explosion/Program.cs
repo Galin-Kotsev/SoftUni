ee893dc61cf14afc9bf7aa46ea94dc7437dcbe70
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-15 00:08:18 +0200

    15.11.2022

---
+++ 2.C# Fundamentals/10.Text Processing/Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string target =
+                Console.ReadLine();
+
+            string newWord = string.Empty;
+
+            int power = 0;
+
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                if (char.IsSymbol(target[i]))
+                {
+                    power += int.Parse(target[i+1].ToString()) + 1;
+
+                    newWord += target[i];
+                }
+
+                if (power == 0)
+                {
+                    newWord += target[i];
+                }
+                else
+                {
+                    power--;
+                }
+            }
+
+            Console.WriteLine(newWord);
+        }
+    }
+}

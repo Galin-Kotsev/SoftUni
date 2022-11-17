9dd73a997706c2d42adc97521f3cabcccbe42f17
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-17 22:17:41 +0200

    17.11.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - LAB/02. Match Phone Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = @"\+\b359( |-)2\1\d{3}\1\d{4}\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(code);
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.Write(string.Join(", ", matches));
+        }
+    }
+}

58c583a7212c9e4a2cb2ccf7a92b965d6920caef
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-17 22:17:41 +0200

    18.11.202217.11.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - LAB/03. Match Dates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code 
+                = @"(?<day>\d{2})([-|.|\/])(?<month>[A-Z][a-z]+)\1(?<year>\d{4})";
+
+            Regex  regex = new Regex(code);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                var day = item.Groups["day"].Value;
+                var month = item.Groups["month"].Value;
+                var year = item.Groups["year"].Value;
+
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
+
+

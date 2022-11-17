0d748e54eb6c94a549bb2c3a88b11cbcdebcda5a
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-17 21:59:20 +0200

    17.11.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - LAB/01. Match Full Name/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = @"\b[A-Z][a-z]+\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(code);
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}

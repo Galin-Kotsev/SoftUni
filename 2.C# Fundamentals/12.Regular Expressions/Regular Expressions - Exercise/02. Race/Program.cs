ac5400cd9feae3cd02c335457d6f6870353fc0d3
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-20 00:15:19 +0200

    20.11.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, int> participants= new Dictionary<string, int>();
+
+            foreach (var name in inputs)
+            {
+                participants.Add(name, 0);
+            }
+
+            string input;
+
+            while((input = Console.ReadLine()) != "end of race")
+            {
+                string name = string.Empty;
+                int km = 0;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (char.IsLetter(input[i]))
+                    {
+                        name += input[i];
+                    }
+                    else if(char.IsDigit(input[i]))
+                    {
+                        km += int.Parse(input[i].ToString());
+                    }
+                }
+
+                if (participants.ContainsKey(name))
+                {
+                    participants[name] += km;
+                }
+
+            }
+
+            int place = 1;
+
+            foreach (var (Key,Value) in participants.OrderByDescending(x => x.Value).Take(3))
+            {
+                string order = string.Empty;
+
+                if (place == 1)
+                {
+                    order = "1st";
+                }
+                else if (place == 2)
+                {
+                    order = "2nd";
+                }
+                else
+                {
+                    order = "3rd";
+                }
+                
+                Console.WriteLine($"{order} place: {Key}");
+                place++;
+
+            }
+        }
+    }
+}
---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - Exercise/03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03._SoftUni_Bar_Income
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern 
+                = @"^[^\|\$\%\.]*\%(?<customer>[A-Z][a-z]+)%[^\|\$\%\.]*\<(?<product>\w+)>[^\|\$\%\.]*\|(?<quantity>\d+)\|[^\|\$\%\.]*?(?<price>\d+(\.\d+)?)\$";
+
+            Regex  regex = new Regex(pattern);
+
+            double income = 0;
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end of shift")
+            {
+
+                MatchCollection match = regex.Matches(input);
+
+                foreach (Match order in match)
+                {
+                    string customer = order.Groups["customer"].Value;
+                    string product = order.Groups["product"].Value;
+                    int quantity = int.Parse(order.Groups["quantity"].Value);
+                    double price = double.Parse(order.Groups["price"].Value);
+
+                    double currPrice = quantity * price;
+                    income += currPrice;
+
+                    Console.WriteLine($"{customer}: {product} - {currPrice:f2}");
+                }
+            }
+
+            Console.WriteLine($"Total income: {income:f2}");
+        }
+    }
+}

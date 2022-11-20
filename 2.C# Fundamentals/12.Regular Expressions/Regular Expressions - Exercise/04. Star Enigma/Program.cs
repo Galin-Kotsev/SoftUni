ab701bf1cfc6e80ac1ff0e208f16e1e98953942d
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-21 00:11:38 +0200

    21.11.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - Exercise/04. Star Enigma/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04._Star_Enigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> planetsAttacked = new List<string>();
+            List<string> planetsDestoyed = new List<string>();
+
+            string pattern 
+                = @"@(?<planet>[A-Z][a-z]+)[^@\-!:>]*\:(?<population>\d+)[^@\-!:>]*!(?<action>[AD])![^@\-!:>]*->(?<soldiers>\d+)";
+            
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+
+                string decryptedMessage = string.Empty;
+
+
+                string message = Console.ReadLine();
+
+                foreach (char letter in message)
+                {
+                    if (letter == 's' || letter == 'a' || letter == 't' || letter == 'r' ||
+                        letter == 'S' || letter == 'A' || letter == 'T' || letter == 'R')
+                    {
+                        count++;
+                    }
+                }
+
+                foreach (char Letter in message)
+                {
+                    int currLetter = Letter - count;
+
+                    decryptedMessage += (char)currLetter;
+                }
+
+                MatchCollection matches = regex.Matches(decryptedMessage);
+
+                foreach (Match actions in matches)
+                {
+                    string planet = actions.Groups["planet"].Value;
+                    string population = actions.Groups["population"].Value;
+                    string action = actions.Groups["action"].Value;
+                    string soldiers = actions.Groups["soldiers"].Value;
+
+                    if (action == "A")
+                    {
+                        planetsAttacked.Add(planet);
+
+                    }
+                    else
+                    {
+                        planetsDestoyed.Add(planet);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Attacked planets: {planetsAttacked.Count}");
+
+            foreach (var planet in planetsAttacked.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+
+            Console.WriteLine($"Destroyed planets: {planetsDestoyed.Count}");
+
+            foreach (var planet in planetsDestoyed.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+
+        
+    }
+}
---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //why did you make me write the pattern !!!
+            
+            string pattern = @"(^|\s)[A-Za-z0-9][\w*\-\.]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]+)+\b";
+
+            Regex regex = new Regex(pattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input); 
+
+            foreach (Match mail in matches)
+            {
+                Console.WriteLine(mail);
+            }
+        }
+    }
+}

164662be4c60c2732820507c54aa7378b7f78d3a
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-23 00:18:28 +0200

    23.11.2022

---
+++ 2.C# Fundamentals/10.Text Processing/Text Processing - Exercise/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _08._Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input
+                = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double score = 0;
+
+            foreach (var word in input)
+            {
+                char firstCh = word[0];
+                char secondCh = word[word.Length - 1];
+                double num = double.Parse(word.Substring(1, word.Length - 2));
+
+                if (firstCh.ToString().ToUpper() == firstCh.ToString())
+                {
+                    score += num / (firstCh - 64);
+                }
+                else if (firstCh.ToString().ToLower() == firstCh.ToString())
+                {
+                    score += num * (firstCh - 96);
+                }
+
+                if (secondCh.ToString().ToUpper() == secondCh.ToString())
+                {
+                    score -= secondCh - 64;
+                }
+                else if (secondCh.ToString().ToLower() == secondCh.ToString())
+                {
+                    score += secondCh - 96;
+                }
+
+            }
+
+            Console.WriteLine($"{score:f2}");
+
+        }
+    }
+}

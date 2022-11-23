984bd7d76c062bf662c6259aa170e32273bf1f3a
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-24 00:47:00 +0200

    24.11.2022

---
+++ 2.C# Fundamentals/13.Exam Preparation/01. PF Final Exam Retake/01. The Imitation Game/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Decode")
+            {
+                string[] inputs = input.Split("|");
+                string command = inputs[0];
+
+                if (command == "ChangeAll")
+                {
+                    string oldCh = inputs[1];
+                    string newCh = inputs[2];
+
+                    encryptedMessage = encryptedMessage.Replace(oldCh, newCh);
+                }
+                else if (command == "Move")
+                {
+                    int n = int.Parse(inputs[1]);
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        string ch = encryptedMessage.First().ToString();
+                        encryptedMessage += ch;
+                        encryptedMessage = encryptedMessage.Substring(1, encryptedMessage.Length - 1);
+                    }
+                }
+                else if (command == "Insert")
+                {
+                    int index = int.Parse(inputs[1]);
+                    string ch = inputs[2];
+
+                    encryptedMessage = encryptedMessage.Insert(index, ch);
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {encryptedMessage}");
+        }
+    }
+}
---
+++ 2.C# Fundamentals/13.Exam Preparation/01. PF Final Exam Retake/02. Ad Astra/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace _02._Ad_Astra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"([#|])(?<item>[A-Za-z ]+)\1(?<expdate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d{1,4})\1";
+
+            int totalCalories = 0;
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection products = regex.Matches(input);
+
+            foreach (Match product in products)
+            {
+                totalCalories += int.Parse(product.Groups["calories"].Value);
+            }
+
+            Console.WriteLine($"You have food to last you for: {totalCalories / 2000} days!");
+
+            foreach (Match item in products)
+            {
+                string product = item.Groups["item"].Value;
+                string expDate = item.Groups["expdate"].Value;
+                int calories = int.Parse(item.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {product}, Best before: {expDate}, Nutrition: {calories}");
+            }
+        }
+
+    }
+}
+

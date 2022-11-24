6d5f99dda3871b1ca1bfda349aa6805ee7624357
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-25 00:00:43 +0200

    25.11.2022

---
+++ 2.C# Fundamentals/13.Exam Preparation/01. PF Final Exam Retake/03. The Pianist/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _03._The_Pianist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string input;
+
+            var ComposerList = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+
+                string[] inputs = input.Split("|");
+
+                string piece = inputs[0];
+                string composer = inputs[1];
+                string key = inputs[2];
+
+                ComposerList.Add(piece, new List<string>() { composer, key });
+            }
+
+            while ((input = Console.ReadLine()) != "Stop")
+            {
+                string[] inputs = input.Split("|");
+
+                string command = inputs[0];
+                
+
+                if (command == "Add")
+                {
+                    string piece = inputs[1];
+                    string composer = inputs[2];
+                    string key = inputs[3];
+
+                    if (!ComposerList.ContainsKey(piece))
+                    {
+                        ComposerList.Add(piece, new List<string>() { composer, key });
+
+                        Console.WriteLine($"{piece} by {composer} in {key} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{piece} is already in the collection!");
+                    }
+                }
+
+                if (command == "Remove")
+                {
+                    string piece = inputs[1];
+                    
+                    if (ComposerList.ContainsKey(piece))
+                    {
+                        Console.WriteLine($"Successfully removed {piece}!");
+                        ComposerList.Remove(piece);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+
+                if (command == "ChangeKey")
+                {
+                    string piece = inputs[1];
+                    string newkey = inputs[2];
+
+                    if (ComposerList.ContainsKey(piece))
+                    {
+                        ComposerList[piece][1] = newkey;
+
+                        Console.WriteLine($"Changed the key of {piece} to {newkey}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {piece} does not exist in the collection.");
+                    }
+                }
+            }
+
+            foreach (var (Key, Value) in ComposerList)
+            {
+                Console.WriteLine($"{Key} -> Composer: {Value[0]}, Key: {Value[1]}");
+            }
+        }
+    }
+}
---
+++ 2.C# Fundamentals/13.Exam Preparation/02. PF Final Exam/01. World Tour/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _01._World_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stops = Console.ReadLine();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Travel")
+            {
+
+                string[] inputs = input.Split(":");
+
+                string command = inputs[0];
+
+                if (command == "Add Stop")
+                {
+                    int index = int.Parse(inputs[1]);
+                    string location = inputs[2];
+
+                    if (index >= 0 && index <= stops.Length - 1)
+                    {
+                        stops = stops.Insert(index, location);
+                    }
+                }
+
+                if (command == "Remove Stop")
+                {
+                    int index = int.Parse(inputs[1]);
+                    int index2 = int.Parse(inputs[2]);
+
+                    if (index >= 0 && index <= stops.Length - 1 && index2 >= 0 && index2 <= stops.Length - 1)
+                    {
+                        stops = stops.Remove(index, index2 - index + 1);
+                    }    
+                }
+
+                if (command == "Switch")
+                {
+                    string oldStop = inputs[1];
+                    string newStop = inputs[2];
+
+                    stops = stops.Replace(oldStop, newStop);
+                }
+
+                Console.WriteLine(stops);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+        }
+    }
+}

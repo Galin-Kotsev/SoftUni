10f5b1549c0e2e21d60d8402c95a1bc9f095fab1
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-28 00:33:38 +0200

    28.11.2022

---
+++ 2.C# Fundamentals/13.Exam Preparation/03.  PF Final Exam Retake/01. Secret Chat/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Reveal")
+            {
+                string[] inputs = input.Split(":|:");
+
+                string command = inputs[0];
+
+                if (command == "InsertSpace")
+                {
+                    int index = int.Parse(inputs[1]);
+
+                    text = text.Insert(index, " ");
+
+                    Console.WriteLine(text);
+                }
+
+                if (command == "Reverse")
+                {
+                    string substring = inputs[1];
+
+                    if (text.Contains(substring))
+                    {
+                        string oldSubstring = substring;
+                        string reversedString = string.Empty;
+
+                        for (int i = substring.Length - 1; i >= 0; i--)
+                        {
+                            reversedString += substring[i];
+                        }
+                        text = ReplaceFirst(text, oldSubstring, "");
+                        
+                        text += reversedString;
+
+                        Console.WriteLine(text);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+
+                    }
+                }
+
+                if (command == "ChangeAll")
+                {
+                    string oldCh = inputs[1];
+                    string newCh = inputs[2];
+
+                    if (text.Contains(oldCh))
+                    {
+                        text = text.Replace(oldCh, newCh);
+
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+
+            Console.WriteLine($"You have a new text message: {text}");
+        }
+
+        public static string ReplaceFirst(string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+    }
+}
---
+++ 2.C# Fundamentals/13.Exam Preparation/03.  PF Final Exam Retake/03. Need for Speed III/Program.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Xml.Linq;
+
+namespace _03._Need_for_Speed_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var carsList = new Dictionary<string, List<int>>();
+
+            string input = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+
+                string[] inputs = input.Split("|");
+
+                string car = inputs[0];
+                int miles = int.Parse(inputs[1]);
+                int fuel = int.Parse(inputs[2]);
+                
+                carsList.Add(car, new List<int>() { miles, fuel });
+            }
+
+            while ((input = Console.ReadLine()) != "Stop")
+            {
+                string[] inputs = input.Split(" : ");
+
+                string command = inputs[0];
+
+                if (command == "Drive")
+                {
+                    string car = inputs[1];
+                    int miles = int.Parse(inputs[2]);
+                    int fuel = int.Parse(inputs[3]);
+
+                    if (carsList.ContainsKey(car))
+                    {
+                        if (carsList[car][1] < fuel)
+                        {
+                            Console.WriteLine("Not enough fuel to make that ride");
+                        }
+                        else if (carsList[car][1] >= fuel)
+                        {
+                            carsList[car][0] += miles;
+                            carsList[car][1] -= fuel;
+
+                            Console.WriteLine($"{car} driven for {miles} kilometers. {fuel} liters of fuel consumed.");
+                        }
+
+                        if (carsList[car][0] >= 100_000)
+                        {
+                            Console.WriteLine($"Time to sell the {car}!");
+
+                            carsList.Remove(car);
+                        }
+                    }
+                }
+
+                if (command == "Refuel")
+                {
+                    string car = inputs[1];
+                    int fuel = int.Parse(inputs[2]);
+
+                    if (carsList.ContainsKey(car))
+                    {
+                        if (carsList[car][1] + fuel < 75)
+                        {
+                            carsList[car][1] += fuel;
+
+                            Console.WriteLine($"{car} refueled with {fuel} liters");
+                        }
+                        else
+                        {
+                            //??
+                            Console.WriteLine($"{car} refueled with {75 - carsList[car][1]} liters");
+                            carsList[car][1] = 75;
+                        }
+                    }
+                }
+
+                if (command == "Revert")
+                {
+                    string car = inputs[1];
+                    int miles = int.Parse(inputs[2]);
+
+                    carsList[car][0] -= miles;
+
+                    Console.WriteLine($"{car} mileage decreased by {miles} kilometers");
+
+                    if (carsList[car][0] < 10_000)
+                    {
+                        carsList[car][0] = 10_000;
+                    }
+                }
+            }
+
+            foreach (var (Key, Value) in carsList)
+            {
+                Console.WriteLine($"{Key} -> Mileage: {Value[0]} kms, Fuel in the tank: {Value[1]} lt.");
+            }
+        }
+    }
+}
+

8518c2f93195b9c9a154701b556e13c530561ca6
Galin-Kotsev <kocevi900701@gmail.com>
2022-11-29 00:10:25 +0200

    28.11.2022

---
+++ 2.C# Fundamentals/13.Exam Preparation/04. PF Final Exam/01. Password Reset/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _01._Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Done")
+            {
+                string[] inputs = input.Split(' ');
+
+                string command = inputs[0];
+
+                if(command == "TakeOdd")
+                {
+                    string oldPassword = password;
+
+                    password = string.Empty;
+
+                    for (int i = 0; i < oldPassword.Length; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            password += oldPassword[i];
+                        }
+                    }
+
+                    Console.WriteLine(password);
+                }
+
+                if (command == "Substitute")
+                {
+                    string oldCh = inputs[1];
+                    string newCh = inputs[2];
+
+                    if (password.Contains(oldCh))
+                    {
+                        password = password.Replace(oldCh, newCh);
+
+                        Console.WriteLine(password);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                }
+
+                if (command == "Cut")
+                {
+                    int index1 = int.Parse(inputs[1]);    
+                    int index2 = int.Parse(inputs[2]);    
+
+                    password = password.Remove(index1, index2);
+
+                    Console.WriteLine(password);
+                }
+
+            }
+
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ 2.C# Fundamentals/13.Exam Preparation/04. PF Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string pattern = @"(@#+)(?<product>[A-Z][A-Za-z\d]{4,}[A-Z])(@#+)";
+            string pattern2 = @"(?<digits>\d)";
+
+            Regex regex = new Regex(pattern);
+            Regex regex2 = new Regex(pattern2);
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                bool invalid = true;
+
+                MatchCollection matches = regex.Matches(input);
+                MatchCollection matches2 = regex2.Matches(input);
+                string newDigit = string.Empty;
+                bool containDigit = false;
+
+                foreach (Match match in matches)
+                {
+                    invalid = false;
+
+                    foreach (Match digit in matches2)
+                    {
+                        string digits = digit.Groups["digits"].Value;
+
+                        for (int k = 0; k < digits.Length; k++)
+                        {
+                            if (digit.Length >= 0)
+                            {
+                                containDigit = true;
+                                newDigit += digits[k]; 
+                            }
+                        }
+                    }
+                }
+
+                if (!invalid)
+                {
+                    if (containDigit)
+                    {
+                        Console.WriteLine($"Product group: {newDigit}");
+                    }
+                    else
+                    {
+                        newDigit = "00";
+                        Console.WriteLine($"Product group: {newDigit}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}

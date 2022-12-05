027f330c27140ac52f79d1c8e77754e6dc4872a0
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-05 21:41:48 +0200

    05.12.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - More Exercise/01. Winning Ticket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace _01._Winning_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([@#$^]{6,10})";
+            Regex regex = new Regex(pattern);
+
+            string[] input = Console.ReadLine().Split(new char[] {',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string ticket = input[i];
+
+                if (ticket.Length == 20)
+                {
+                    string leftTickedStr = ticket.Substring(0, 10);
+                    string rightTickedStr = ticket.Substring(10, 10);
+                    string leftTicked = string.Empty;
+                    string rightTicked= string.Empty;
+
+                    Match match = regex.Match(leftTickedStr);
+                    if (match.Success)
+                    {
+                        leftTicked = match.Groups[1].Value;
+                    }
+
+                    Match match2 = regex.Match(rightTickedStr);
+                    if (match2.Success)
+                    {
+                        rightTicked = match2.Groups[1].Value;
+                    }
+
+                    if (leftTicked.Length >=6 && rightTicked.Length >= 6 && (rightTicked.Contains(leftTicked) || leftTicked.Contains(rightTicked)))
+                    {
+                        if (leftTicked.Length + rightTicked.Length == 20)
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - {rightTicked.Length}{rightTicked.First()} Jackpot!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ticket \"{ticket}\" - {rightTicked.Length}{rightTicked.First()}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{ticket}\" - no match");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+            }
+        }
+    }
+}
+
---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - More Exercise/2. Rage Quit/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace _2._Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letterPattern = @"([^\d]+)";
+            string digitsPattern = @"([\d]+)";
+            List<char> unique = new List<char>();
+
+            Regex letter = new Regex(letterPattern);
+            Regex digit = new Regex(digitsPattern);
+
+            string input = Console.ReadLine().ToUpper();
+
+            MatchCollection letters = letter.Matches(input);
+            MatchCollection digits = digit.Matches(input);
+
+            string result = string.Empty;
+
+            if (letters.Any())
+            {
+                if (digits.Any())
+                {
+                    for (int i = 0; i < letters.Count; i++)
+                    {
+                        string charecter = letters[i].Value;
+                        int n = int.Parse(digits[i].Value);
+
+                        for (int l = 0; l < charecter.Length; l++)
+                        {
+                            if (!unique.Contains(charecter[l]))
+                            {
+                                unique.Add(charecter[l]);
+                            }
+                        }
+
+                        for (int k = 1; k <= n; k++)
+                        {
+                            result += charecter.ToString();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Unique symbols used: {unique.Count}");
+            Console.WriteLine(result);
+        }
+    }
+}

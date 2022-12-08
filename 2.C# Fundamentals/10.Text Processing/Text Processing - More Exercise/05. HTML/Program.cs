fb9a889d00932e720c4f3681b8aae6474c7254b7
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-08 21:15:28 +0200

    08.12.2022

---
+++ 2.C# Fundamentals/10.Text Processing/Text Processing - More Exercise/05. HTML/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _05._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            string comment;
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"    {title}");
+            Console.WriteLine("</h1>");
+            Console.WriteLine("<article>");
+            Console.WriteLine($"    {content}");
+            Console.WriteLine("</article>");
+
+
+            while ((comment = Console.ReadLine()) != "end of comments")
+            {
+                Console.WriteLine("<div>");
+                Console.WriteLine($"    {comment}");
+                Console.WriteLine("</div>");
+            }
+        }
+    }
+}

2015a96b5c905d0a39b9fbed04c0f729ed7c305f
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-08 22:56:22 +0200

    08.12.2022

--- 2.C# Fundamentals/10.Text Processing/Text Processing - More Exercise/03. Treasure Finder/Program.cs
+++ 2.C# Fundamentals/10.Text Processing/Text Processing - More Exercise/03. Treasure Finder/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _03._Treasure_Finder
 {
@@ -6,7 +9,51 @@ namespace _03._Treasure_Finder
     {
         static void Main(string[] args)
         {
-           
+            List<int> keys
+                = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string orePattern = @"([\&])(?<ore>\w*)\1";
+            string locationPattern = @"[<](?<location>\w*)[>]";
+
+            Regex regex = new Regex(orePattern);
+            Regex regex1 = new Regex(locationPattern);
+
+            string message;
+
+            var treasure
+                = new Dictionary<string, string>();
+
+            while ((message = Console.ReadLine()) != "find")
+            {
+                string decryptedMessage = string.Empty;
+
+                for (int i = 0; i < message.Length; i++)
+                {
+                    if (i >= keys.Count)
+                    {
+                        keys.AddRange(keys);
+                    }
+
+                    decryptedMessage += (char)(message[i] - keys[i]);
+                }
+
+                Match ore = regex.Match(decryptedMessage);
+                Match location = regex1.Match(decryptedMessage);
+
+                if (ore.Success && location.Success)
+                {
+                    string ores = ore.Groups["ore"].Value;
+                    string locations = location.Groups["location"].Value;
+
+                    treasure.Add(ores, locations);
+                }
+            }
+
+            foreach (var (Key, Value) in treasure)
+            {
+                Console.WriteLine($"Found {Key} at {Value}");
+            }
+
         }
     }
 }

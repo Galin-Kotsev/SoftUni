88d007bfed32183831560db961b5becc2ae701bc
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-09 23:39:19 +0200

    09.12.2022

---
+++ 2.C# Fundamentals/05.List/Lists - More Exercise/03. Take-Skip Rope/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Take_Skip_Rope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<int> take = new List<int>();
+            List<int> skip = new List<int>();
+            List<int> numbers = new List<int>();
+            List<string> text = new List<string>();
+
+            string hiddenMessage = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    numbers.Add(int.Parse(input[i].ToString()));
+                }
+                else if (char.IsLetter(input[i]))
+                {
+                    text.Add(input[i].ToString());
+                }
+                else
+                {
+                    text.Add(input[i].ToString());
+                }
+            }
+
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    take.Add(int.Parse(numbers[i].ToString()));
+                }
+                else
+                {
+                    skip.Add(int.Parse(numbers[i].ToString()));
+                }
+            }
+
+            int totalSkipped = 0;
+
+            for (int i = 0 ; i < take.Count; i++)
+            {
+                int currTake = take[i];
+                int currSkip = skip[i];
+
+                hiddenMessage += string.Join("", text.Skip(totalSkipped).Take(currTake));
+
+                totalSkipped += currSkip+currTake;
+            }
+
+            Console.WriteLine(hiddenMessage.ToString());
+        }
+    }
+}
---
+++ 2.C# Fundamentals/05.List/Lists - More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+
+            List<int> drumSet 
+                = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> drumSet2 = new List<int>();
+
+            drumSet2.AddRange(drumSet);
+
+            string input = string.Empty;
+
+            while((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                string[] inputs = input.Split();
+
+                int power = int.Parse(inputs[0]);
+
+                for (int i = 0; i < drumSet.Count; i++)
+                {
+                    drumSet[i] -= power;
+                    
+                    if (drumSet[i] <= 0)
+                    {
+                        if (savings >= drumSet2[i] * 3 && savings > 0)
+                        {
+                            drumSet[i] = drumSet2[i];
+                            
+                            int newDrum = drumSet2[i] * 3;
+
+                            savings -= newDrum;
+                        }
+                        else
+                        {
+                            drumSet.RemoveAt(i);
+                            drumSet2.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine(String.Join(" ", drumSet));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}

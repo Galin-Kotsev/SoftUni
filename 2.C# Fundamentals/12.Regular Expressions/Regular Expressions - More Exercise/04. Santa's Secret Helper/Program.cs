1cb479fb5a75321a2440f47d6abf642f9b8809a1
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-11 21:23:52 +0200

    11.12.2022

---
+++ 2.C# Fundamentals/12.Regular Expressions/Regular Expressions - More Exercise/04. Santa's Secret Helper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _04._Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = @"[@](?<kidName>[A-Za-z]+)[^\@\-\!\:\>]*(?<behavior>!G!)";
+
+            int n = int.Parse(Console.ReadLine());
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string decrypteMessage = string.Empty;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    decrypteMessage += char.Parse(((char)(input[i] - n)).ToString());
+                }
+
+                Regex regex = new Regex(name);
+
+                Match matchName = regex.Match(decrypteMessage);
+
+                if (matchName.Success)
+                {
+                    string kidName = string.Empty;
+                    kidName = matchName.Groups["kidName"].Value;
+                    Console.WriteLine(kidName);
+                }
+            }
+        }
+    }
+}

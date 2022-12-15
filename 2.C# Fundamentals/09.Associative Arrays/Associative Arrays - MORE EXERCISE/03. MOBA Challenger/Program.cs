43383b34ca267e25b43ad092493aa8354ad3bfc6
Galin-Kotsev <kocevi900701@gmail.com>
2022-12-16 00:13:01 +0200

    16.12.2022

--- 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - MORE EXERCISE/03. MOBA Challenger/Program.cs
+++ 2.C# Fundamentals/09.Associative Arrays/Associative Arrays - MORE EXERCISE/03. MOBA Challenger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._MOBA_Challenger
 {
@@ -6,7 +8,118 @@ namespace _03._MOBA_Challenger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+
+            List<Player> PlayerPool = new List<Player>();
+
+            while ((input = Console.ReadLine()) != "Season end")
+            {
+                if (input.Contains("vs"))
+                {
+                    string[] inputs = input.Split(" ");
+                    string player1Name = inputs[0];
+                    string player2Name = inputs[2];
+
+                    Player player1
+                        = PlayerPool.FirstOrDefault(x => x.PlayerName == player1Name);
+
+                    Player player2
+                        = PlayerPool.FirstOrDefault(x => x.PlayerName == player2Name);
+
+                    if (player1 != null && player2 != null)
+                    {
+                        bool haveSameSkill = false;
+
+                        foreach (var position in player1.Positions)
+                        {
+                            foreach (var position2 in player2.Positions)
+                            {
+                                if (position.Key == position2.Key)
+                                {
+                                    haveSameSkill = true;
+                                }
+                            }
+                        }
+
+                        if (haveSameSkill)
+                        {
+                            if (player1.TotalSkillPoints > player2.TotalSkillPoints)
+                            {
+                                PlayerPool.Remove(player2);
+                            }
+                            else if (player1.TotalSkillPoints < player2.TotalSkillPoints)
+                            {
+                                PlayerPool.Remove(player1);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    string[] inputs = input.Split(" -> ");
+                    string playerName = inputs[0];
+                    string position = inputs[1];
+                    int skillPoints = int.Parse(inputs[2]);
+
+                    Player player
+                        = PlayerPool.FirstOrDefault(x => x.PlayerName == playerName);
+
+                    if (player == null)
+                    {
+                        PlayerPool.Add(new Player(playerName, position, skillPoints));
+                    }
+                    else
+                    {
+                        if (player.Positions.ContainsKey(position))
+                        {
+                            if (player.Positions[position] < skillPoints)
+                            {
+                                player.Positions[position] = skillPoints;
+                            }
+                        }
+                        else
+                        {
+                            player.Positions.Add(position, skillPoints);
+                        }
+                    }
+                }
+            }
+
+            foreach (var player in PlayerPool.OrderByDescending(x => x.TotalSkillPoints))
+            {
+                Console.WriteLine($"{player.PlayerName}: {player.TotalSkillPoints} skill");
+
+                foreach (var position in player.Positions.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"- {position.Key} <::> {position.Value}");
+                }
+            }
         }
+    }
+}
+public class Player
+{
+    public Player(string playerName, string position, int skillPoints)
+    {
+        PlayerName = playerName;
+        Positions = new Dictionary<string, int>
+        {
+            { position, skillPoints }
+        };
+    }
+
+    public string PlayerName { get; set; }
+    public Dictionary<string, int> Positions { get; set; }
+    public int TotalSkillPoints => SkillPointsCalc();
+
+    public int SkillPointsCalc()
+    {
+        int sum = 0;
+
+        foreach (var item in Positions)
+        {
+            sum += item.Value;
+        }
+        return sum;
     }
 }

c9668bf4c3980d7488bddb5dc2ca041cbf4c5731
Galin-Kotsev <kocevi900701@gmail.com>
2023-01-10 23:17:18 +0200

    10.01.2023

---
+++ 3. C# Advanced/1.Stacks and Queues/Stacks and Queues - LAB/01. Reverse a String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var charectar in input)
+            {
+                stack.Push(charectar);
+            }
+
+            Console.WriteLine(String.Join("", stack));
+        }
+    }
+}

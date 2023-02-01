7635a5cfd792b2e6da257580af333833fafc7502
Galin-Kotsev <kocevi900701@gmail.com>
2023-02-02 00:03:09 +0200

    01.02.2023

---
+++ 3. C# Advanced/06.OOP/OOP - Exercise/01. Define a Class Person/Family.cs
@@ -0,0 +1,51 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people = new List<Person>();
+
+        public Family() 
+        { 
+            people = new List<Person>();
+        }
+
+        public void AddMember()
+        {
+                string[] inputs = Console.ReadLine().Split(" ");
+                string name = inputs[0];
+                int age = int.Parse(inputs[1]);
+                Person person = new Person(name, age);
+                people.Add(person);
+        }
+
+        public void GetOldestMember()
+        {
+            Console.WriteLine(string.Join(" ", people.OrderByDescending(x => x.Age).Take(1)));
+        }
+
+        public void GetOldestMembers()
+        {
+            foreach (Person person in people.OrderBy(x => x.Name))
+            {
+
+                if (person.Age > 30)
+                {
+                    Console.WriteLine($"{person.Name} - {person.Age}");
+                }
+            }
+            
+        }
+
+    }
+}
+
+
+
---
+++ 3. C# Advanced/06.OOP/OOP - Exercise/01. Define a Class Person/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //Person person 
+                //= new Person("Peter", 20);
+            //Person person2 
+               // = new Person("George", 19);
+            //Person person3 
+                //= new Person("Jose", 43);
+                
+           
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                family.AddMember();
+            }
+
+            family.GetOldestMembers();
+        }
+    }
+
+}

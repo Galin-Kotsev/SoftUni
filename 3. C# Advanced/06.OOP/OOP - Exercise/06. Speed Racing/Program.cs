0430b8c513277cb2e08b45113bcf3b5206be060c
Galin-Kotsev <kocevi900701@gmail.com>
2023-02-03 00:26:32 +0200

    03.02.2023

--- 3. C# Advanced/06.OOP/OOP - Exercise/06. Speed Racing/Program.cs
+++ 3. C# Advanced/06.OOP/OOP - Exercise/06. Speed Racing/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Car> cars 
+                = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputs 
+                    = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = inputs[0];
+                double fuelAmount = double.Parse(inputs[1]);
+                double fuelConsumptionPerKilometer = double.Parse(inputs[2]);
+
+                cars.Add(new Car(model,fuelAmount,fuelConsumptionPerKilometer));
+            }
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string command = inputs[0];
+                string model = inputs[1];
+                double travelledDistance = double.Parse(inputs[2]); 
+
+                if(command == "Drive")
+                {
+                    Car car = cars.FirstOrDefault(c => c.Model == model);
+                 
+                    car.Drive(travelledDistance);
+                }
+            }
+
+            foreach  (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
+
---
+++ 3. C# Advanced/06.OOP/OOP - Exercise/08.CarSalesman/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.CarSalesman
+{
+    public class Car
+    {
+        private string model;
+        private int weight;
+        private string color;
+        private Engine engine;
+
+        public Car(string model, int weight, string color, Engine engine, string model, int weight, string color, Engine engine)
+        {
+            Model = model;
+            Weight = weight;
+            Color = color;
+            Engine = engine;
+            Model = model;
+            Weight = weight;
+            Color = color;
+            Engine = engine;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+    }
+}

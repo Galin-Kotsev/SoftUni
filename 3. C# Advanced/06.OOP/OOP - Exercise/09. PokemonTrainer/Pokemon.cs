a04b14fee28f9b2b40c094b0a5b31ee0da3cdf09
Galin-Kotsev <kocevi900701@gmail.com>
2023-02-07 23:58:49 +0200

    07.02.2023

---
+++ 3. C# Advanced/06.OOP/OOP - Exercise/09. PokemonTrainer/Pokemon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._PokemonTrainer
+{
+    public class Pokemon
+    {
+        private string pokemonName;
+        private string element;
+        private int health;
+
+        public Pokemon(string pokemonName, string element, int health)
+        {
+            PokemonName = pokemonName;
+            Element = element;
+            Health = health;
+        }
+
+        public string PokemonName
+        {
+            get { return pokemonName; }
+            set { pokemonName = value; }
+        }
+        public string Element
+        {
+            get { return element; }
+            set { element = value; }
+        }
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+    }
+}
---
+++ 3. C# Advanced/06.OOP/OOP - Exercise/09. PokemonTrainer/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _09._PokemonTrainer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        List<Trainer> trainers 
+            = new List<Trainer>();
+
+        string input = String.Empty;
+
+        while ((input = Console.ReadLine()) != "Tournament")
+        {
+            string[] inputs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = inputs[0];
+            string pokemonName = inputs[1];
+            string element = inputs[2];
+            int health = int.Parse(inputs[3]);
+
+            if (!trainers.Any(x=> x.Name == name))
+            {
+                trainers.Add(new Trainer(name));
+            }
+
+            trainers.FirstOrDefault(x => x.Name == name)
+                .Pokemons.Add(new Pokemon(pokemonName, element, health));
+        }
+
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string element = input;
+
+            foreach (var trainer in trainers)
+            {
+                bool fightWon = false;
+
+                foreach (var pokemon in trainer.Pokemons)
+                {
+                    if (pokemon.Element == element)
+                    {
+                        fightWon = true;
+                        continue;
+                    }
+                }
+
+                if (fightWon)
+                {
+                    trainer.NumberOfBadges++;
+                }
+                else
+                {
+                    for (int i = 0; i < trainer.Pokemons.Count; i++)
+                    {
+                        if (trainer.Pokemons[i].Health <= 0)
+                        {
+                            trainer.Pokemons.Remove(trainer.Pokemons[i]);
+                            i--;
+                        }
+                        else
+                        {
+                            trainer.Pokemons[i].Health -= 10;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        foreach (Trainer trainer in trainers.OrderByDescending(x=>x.NumberOfBadges)) 
+        {
+            Console.WriteLine($"{trainer.Name} {trainer.NumberOfBadges} {trainer.Pokemons.Count}");
+        
+        }
+    }
+}

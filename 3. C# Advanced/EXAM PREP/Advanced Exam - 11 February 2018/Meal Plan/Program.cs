a251d91caddf6a546433579d811df1c771a7999a
Galin-Kotsev <kocevi900701@gmail.com>
2023-02-17 23:41:37 +0200

    17.02.2023

---
+++ 3. C# Advanced/EXAM PREP/Advanced Exam - 11 February 2018/Meal Plan/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Meal_Plan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> meals
+                = new Queue<string>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries));
+
+            Stack<int> calories
+                = new Stack<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int currDay = calories.Peek();
+
+            int totalMeals = 0;
+
+            while (true)
+            {
+                if (meals.Count == 0 || calories.Count == 0)
+                {
+                    break;
+                }
+
+                totalMeals++;
+
+                string meal = meals.Dequeue();
+
+                if (meal == "salad")
+                {
+                    currDay -= 350;
+                }
+
+                if (meal == "soup")
+                {
+                    currDay -= 490;
+                }
+
+                if (meal == "pasta")
+                {
+                    currDay -= 680;
+                }
+
+                if (meal == "steak")
+                {
+                    currDay -= 790;
+                }
+
+                if (currDay <= 0)
+                {
+                    calories.Pop();
+
+                    if (calories.Count == 0 )
+                    {
+                        break;
+                    }
+
+                    currDay += calories.Peek();
+                }
+
+            }
+
+            List<string> mealtsLeft = new List<string>();
+
+            foreach (string meal in meals)
+            {
+                mealtsLeft.Add(meal);
+            }
+
+            if (meals.Count == 0)
+            {
+                Console.WriteLine($"John had {totalMeals} meals.");
+                Console.WriteLine($"For the next few days, he can eat {currDay}, {string.Join(", ", calories.Skip(1))} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {totalMeals} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", mealtsLeft)}.");
+
+            }
+        }
+    }
+}

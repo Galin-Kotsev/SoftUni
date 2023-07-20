8c496dc9436bb8771a764a1b5efe27b1e685274d
Galin-Kotsev <kocevi900701@gmail.com>
2023-07-20 22:01:28 +0300

    20.07.2023

--- 6.JS Front-End/04.DOM and Events/EXERCISE/06. Table - Search Engine/solution.js
+++ 6.JS Front-End/04.DOM and Events/EXERCISE/06. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchedText = document.querySelector("#searchField").value;
+      const tableList = Array.from(document.querySelectorAll("tbody td"))
 
+      tableList.forEach(word => {
+         word.parentElement.classList.remove("select")
+      });
+
+      tableList.forEach(word => {
+         if (word.textContent.includes(searchedText)) {
+            console.log(word)
+            word.parentElement.classList.add("select")
+         }
+      });
    }
 }
--- 6.JS Front-End/04.DOM and Events/EXERCISE/07. Format the Text/solution.js
+++ 6.JS Front-End/04.DOM and Events/EXERCISE/07. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  const input = document.querySelector("#input").value.split(".");
+  input.pop();
+  const output = document.querySelector("#output");
+
+  while (input.length > 0) {
+
+    const p = document.createElement("p");
+    p.textContent = input.splice(0, 3).join(".").trim();
+    output.appendChild(p);
+
+  }
 }
+
+
+
+
